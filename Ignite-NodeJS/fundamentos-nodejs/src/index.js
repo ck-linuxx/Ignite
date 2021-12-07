5f5775b5ade373c03be5bc4f64f5813ae544f918
Clark <amorimclark@gmail.com>
2021-12-06 21:39:17 -0300

    1.Criando a estrutura do projeto

--- Ignite-NodeJS/fundamentos-nodejs/src/index.js
+++ Ignite-NodeJS/fundamentos-nodejs/src/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (request, response) => {
-  return response.send("Hello World")
+  return response.json({message: "Hello World Ignite!"})
 })
 
 app.listen(3333)
