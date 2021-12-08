817218761b6b8bd6cb360359f01018e92f1f6078
Clark <amorimclark@gmail.com>
2021-12-08 20:46:47 -0300

    3. Utilizando os métodos HTTP

--- Ignite-NodeJS/fundamentos-nodejs/src/index.js
+++ Ignite-NodeJS/fundamentos-nodejs/src/index.js
@@ -2,8 +2,34 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (request, response) => {
-  return response.json({message: "Hello World Ignite!"})
+app.get("/courses", (request, response) => {
+  return response.json([
+    "Curso 1", "Curso 2", "Curso 3"
+  ])
+})
+
+app.post("/courses", (request, response) => {
+  return response.json([
+    "Curso 1", "Curso 2", "Curso 3", "Curso 4",
+  ])
+})
+
+app.put("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6", "Curso 2", "Curso 3", "Curso 4"
+  ])
+})
+
+app.patch("/courses/:id", (request, respponse) => {
+  return response.json([
+    "Curso 6", "Curso 7", "Curso 3", "Curso 4"
+  ])
+})
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6", "Curso 2", "Curso 4"
+  ])
 })
 
 app.listen(3333)
