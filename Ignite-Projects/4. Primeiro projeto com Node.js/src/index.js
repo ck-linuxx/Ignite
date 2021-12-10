e03fd530b1a14a9e0cc0532e56795f5286e60b57
Clark <amorimclark@gmail.com>
2021-12-10 09:47:10 -0300

    3. Validando CPF existente

--- Ignite-Projects/4. Primeiro projeto com Node.js/src/index.js
+++ Ignite-Projects/4. Primeiro projeto com Node.js/src/index.js
@@ -5,17 +5,23 @@ const app = express()
 
 app.use(express.json())
 
-const costumers = [] //db
+const costumers = [] //database
 
 //criação da conta
 app.post("/account", (request,response) => {
   const { cpf, name } = request.body
-  const id = uuidv4()
+  const custumerAlreadyExists = costumers.some(
+      (customer) => customer.cpf === cpf
+    )   //some(): faz uma busca e retorna true or false
+
+    if(custumerAlreadyExists){
+      return response.status(400).json({ error: "Customer already exists!" })
+    }
 
   costumers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: []
   })
 
