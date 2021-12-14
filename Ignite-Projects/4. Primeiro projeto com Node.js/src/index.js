b962393e9532195d1d35d07d378da430dfa69aa2
Clark <amorimclark@gmail.com>
2021-12-13 22:28:19 -0300

    4. Listando extrato

--- Ignite-Projects/4. Primeiro projeto com Node.js/src/index.js
+++ Ignite-Projects/4. Primeiro projeto com Node.js/src/index.js
@@ -5,12 +5,12 @@ const app = express()
 
 app.use(express.json())
 
-const costumers = [] //database
+const custumers = [] //database
 
 //criação da conta
 app.post("/account", (request,response) => {
   const { cpf, name } = request.body
-  const custumerAlreadyExists = costumers.some(
+  const custumerAlreadyExists = custumers.some(
       (customer) => customer.cpf === cpf
     )   //some(): faz uma busca e retorna true or false
 
@@ -18,7 +18,7 @@ app.post("/account", (request,response) => {
       return response.status(400).json({ error: "Customer already exists!" })
     }
 
-  costumers.push({
+  custumers.push({
     cpf,
     name,
     id: uuidv4(),
@@ -27,6 +27,14 @@ app.post("/account", (request,response) => {
 
   return response.status(201).send()
 
+})
+
+app.get("/statement/:cpf", (request, response) => {
+  const {cpf} = request.params
+
+  const customer = custumers.find(customer => customer.cpf === cpf)
+
+  return response.json(customer.statement)
 })
 
 app.listen(3333)
