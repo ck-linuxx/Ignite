11ca9241a1a051b85d8008b96fd91c9f9d67a458
Clark <amorimclark@gmail.com>
2021-12-16 09:50:58 -0300

    7. Criando depósito na conta

--- Ignite-Projects/4. Primeiro projeto com Node.js/src/index.js
+++ Ignite-Projects/4. Primeiro projeto com Node.js/src/index.js
@@ -49,4 +49,22 @@ app.get("/statement",verifyIfExistAccountCPF, (request, response) => {
   return response.json(customer.statement)
 })
 
+app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
+  const { description, amount } = request.body
+
+  const { customer } = request
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+
+})
+
 app.listen(3333)
