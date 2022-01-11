9e94dd0f885cd6bf941b59a8730bd9f1aeafbf7e
Clark <amorimclark@gmail.com>
2022-01-10 22:40:07 -0300

    6.Lendo os dados do upload

--- Ignite-NodeJS/criando a api com nodejs/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Ignite-NodeJS/criando a api com nodejs/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,16 +1,43 @@
 import {parse} from "csv-parse"
 import fs from "fs" //file system
+import { resolve } from "path";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository"
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void{ //recebendo o arquivo
-    const stream = fs.createReadStream(file.path) //leitura do arquivo em partes
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    const parseFile = parse()
-    stream.pipe(parseFile) //dentro da função coloca
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{ //fazer somente a leitura das categorias
+    return new Promise((resolve,reject) =>{
+      const stream = fs.createReadStream(file.path) //leitura do arquivo em partes
+      const categories:IImportCategory[] = []
 
-    parseFile.on("data", async (line) => {
-      console.log(line)
+      const parseFile = parse()
+      stream.pipe(parseFile) //dentro da função coloca
+
+      parseFile.on("data", async (line) => {
+        const [ name, description ] = line 
+        categories.push({
+          name,
+          description,
+        })
+      })
+      .on("end", () => {
+        resolve(categories)
+      })
+      .on("error", (err) =>{
+        reject(err)
+      })
     })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void>{ //recebendo o arquivo
+    const categories = await this.loadCategories(file)
+    console.log(categories)
   }
 }
 
