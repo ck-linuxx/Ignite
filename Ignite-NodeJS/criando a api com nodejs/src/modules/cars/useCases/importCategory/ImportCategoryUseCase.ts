ce8ce48ef53f38c00474a7f62fee5fcaa1e3a153
Clark <amorimclark@gmail.com>
2022-01-10 22:14:46 -0300

    4.Conhecendo o conceito de stream

--- Ignite-NodeJS/criando a api com nodejs/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Ignite-NodeJS/criando a api com nodejs/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,16 @@
+import {parse} from "csv-parse"
+import fs from "fs" //file system
+
 class ImportCategoryUseCase {
-  execute(file: any){
-    console.log(file)
+  execute(file: Express.Multer.File): void{ //recebendo o arquivo
+    const stream = fs.createReadStream(file.path) //leitura do arquivo em partes
+
+    const parseFile = parse()
+    stream.pipe(parseFile) //dentro da função coloca
+
+    parseFile.on("data", async (line) => {
+      console.log(line)
+    })
   }
 }
 
