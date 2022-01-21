b78f715a9c190580a639887e6cd22d0eda432c3d
Clark <amorimclark@gmail.com>
2022-01-20 23:43:19 -0300

    1. Conhecendo as formas de usar o banco de dados

--- Ignite-NodeJS/rentalx/src/server.ts
+++ Ignite-NodeJS/rentalx/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express"
-import { routes } from "./routes";
 
+import { routes } from "./routes";
 import swaggerFile from "./swagger.json"
 
-const app = express()
+import "./database"
 
+const app = express()
+ 
 app.use(express.json())
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
